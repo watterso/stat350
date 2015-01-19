c9977411267ee60dd1e7a33c41115db1c9e4f5eb
James Watterson <jameslwatterson@gmail.com>
2015-01-18 19:12:13 -0500

    lab1

---
+++ lab1/lab1.R
@@ -0,0 +1,9 @@
+library(lattice)
+iqs = read.table('eg01-14iq.txt', header=TRUE)
+histogram(~IQ, iqs, main = "Histogram of IQs", col="blue")
+
+sev = read.table('ex01-44sevengr.txt', header=TRUE)
+histogram(~ID, sev, main="Histogram of IDs", col="blue")
+histogram(~GPA, sev, main="Histogram of GPAs", col="blue")
+histogram(~IQ, sev, main="Histogram of IQs", col="blue")
+histogram(~SelfConcept, sev, main="Histogram of Self Concept", col="blue")
