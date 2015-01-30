2b3a6b0d39330ae19bc2eae2e28599c07e0be0c4
James Watterson <jameslwatterson@gmail.com>
2015-01-30 15:33:59 -0500

    corrected assignment operator from '=' to '<-'

--- lab1/lab1.R
+++ lab1/lab1.R
@@ -1,8 +1,8 @@
 library(lattice)
-iqs = read.table('eg01-14iq.txt', header=TRUE)
+iqs <- read.table('eg01-14iq.txt', header=TRUE)
 histogram(~IQ, iqs, main = "Histogram of IQs", col="blue")
 
-sev = read.table('ex01-44sevengr.txt', header=TRUE)
+sev <- read.table('ex01-44sevengr.txt', header=TRUE)
 histogram(~ID, sev, main="Histogram of IDs", col="blue")
 histogram(~GPA, sev, main="Histogram of GPAs", col="blue")
 histogram(~IQ, sev, main="Histogram of IQs", col="blue")
