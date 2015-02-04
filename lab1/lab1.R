30b7cf40612873a2f934f52bfdf4f848f7f58fea
James Watterson <jameslwatterson@gmail.com>
2015-02-03 21:28:53 -0500

    updated code to use sections

--- lab1/lab1.R
+++ lab1/lab1.R
@@ -1,8 +1,11 @@
 library(lattice)
+## ---- A.1
 iqs <- read.table('eg01-14iq.txt', header=TRUE)
+## ---- A.2
 histogram(~IQ, iqs, main = "Histogram of IQs", col="blue")
-
+## ---- B.1
 sev <- read.table('ex01-44sevengr.txt', header=TRUE)
+## ---- B.2
 histogram(~ID, sev, main="Histogram of IDs", col="blue")
 histogram(~GPA, sev, main="Histogram of GPAs", col="blue")
 histogram(~IQ, sev, main="Histogram of IQs", col="blue")
--- lab2/lab2.R
+++ lab2/lab2.R
@@ -1,31 +1,32 @@
 library(lattice)
-#--=PART A=--
+## ---- A
 d_data <- read.table("earthdensity.txt", header=TRUE)
-# A.1
+## ---- A.1
 d_fnum <- fivenum(d_data[,1])
-# A.2
+d_fnum
+## ---- A.2
 bwplot(d_data[,1], xlab="Water Density Coefficient")
-# A.3
+## ---- A.3
 d_mean <- mean(d_data[,1])
 d_sd <- sd(d_data[,1])
 d_median <- median(d_data[,1])
-# yes the median is close to the mean, it is within 1 standard deviation
-# A.4
-water_density <- 999.97
+c(d_mean,d_sd,d_median)
+## ---- A.4
+water_density <- 999.97               # in kg/m^3
 earth_density <- d_mean*water_density
-# mean and median are so close together you can use either value
-# to estimate the actual density of the earth
-#--=PART B=--
+earth_density
+## ---- B
 crp <- read.table("ex01-73crp.txt", header=TRUE)
-# B.1
+## ---- B.1
 f_num <- fivenum(crp[,1])
-# B.2
+f_num
+## ----B.2
 bwplot(crp[,1], xlab="CRP Concentration (mg/l)")      #right skewed
-# B.3
+## ---- B.3
 iqr <- f_num[4]-f_num[2]
 min_out <- f_num[2] - 1.5*iqr
 max_out <- f_num[4] + 1.5*iqr
 outliers <- c(crp[crp$CRP < min_out,], crp[crp$CRP > max_out,])
-# B.4
+outliers
+## ---- B.4
 histogram(~CRP,crp, xlab="CRP Concentration (mg/l)", col="blue")
-# yes IQR rule properly highlights outliers
