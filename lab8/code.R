283a1ff922166953a2b8009ec383f54bd61745a7
James Watterson <jameslwatterson@gmail.com>
2015-04-22 17:31:35 -0400

    up to #6 done on lab8

---
+++ lab8/code.R
@@ -0,0 +1,18 @@
+## ---- Setup
+library(lattice)
+lin_reg_plot <- function(data, ...){
+  xyplot(data,
+         panel = function(x,y){
+          panel.xyplot(x,y)
+          panel.lmline(x,y)
+         },...)
+}
+sales <- read.table("sales.txt", header=T)
+## ---- A_1
+lin_reg_plot(sales$SalesPrice ~ sales$AssessedValue, xlab="Assessed Value", ylab="Sales Price")
+## ---- A_3
+sales.cor <- cor(sales$SalesPrice, sales$AssessedValue)
+## ---- A_5
+sales.lm <- lm(sales$SalesPrice ~ sales$AssessedValue)
+## ---- A_6
+sales.predict <- predict(sales.lm)
